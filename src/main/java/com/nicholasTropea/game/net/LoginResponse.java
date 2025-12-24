5408b2255961830fb57be37d74bf76eedc0d0d71
nicholasTropea <nicholastropea042@gmail.com>
2025-12-24 10:44:24 +0100

    Update: Completed LoginResponse - Improved and completed JavaDoc comments - Re-implemented validation and made it minimal

--- src/main/java/com/nicholasTropea/game/net/LoginResponse.java
+++ src/main/java/com/nicholasTropea/game/net/LoginResponse.java
@@ -4,9 +4,10 @@ import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 /**
- * Risposta ad una richiesta di login di un giocatore.
+ * Risposta ad una richiesta di {@link LoginRequest}.
  * 
  * JSON atteso:
+ * <pre>{@code
  * {
  *    "success" : BOOLEAN,
  *    "error" : STRING,
@@ -17,29 +18,42 @@ import java.util.List;
  *    "errors" : INT,
  *    "score" : INT
  * }
+ * }</pre>
+ * 
+ * Errori possibili: "nome inesistente", "psw incorretta"...
+ * 
+ * @see RegisterResponse per il formato di registrazione
  */
 public class LoginResponse {
+    /** true se login avvenuto con successo, false altrimenti */
     @SerializedName("success")
     private final boolean success;
 
+    /** Messaggio d'errore (null se success=true) */
     @SerializedName("error")
-    private final String error; // null se success = true
+    private final String error;
 
+    /** Id della partita corrente (null se success=false) */
     @SerializedName("gameId")
     private final Integer gameId;
 
+    /** Lista di parole della partita corrente (null se success=false) */
     @SerializedName("words")
     private final List<String> words;
 
+    /** Lista di gruppi di parole già indovinate della partita corrente */
     @SerializedName("correctGroups")
-    private final List<List<String>> correctGroups; // Gruppi già indovinati
+    private final List<List<String>> correctGroups;
 
+    /** Tempo rimanente della partita corrente in millisecondi */
     @SerializedName("timeLeft")
-    private final Long timeLeft; // ms
+    private final Long timeLeft;
 
+    /** Numero di errori già commessi nella partita corrente*/
     @SerializedName("errors")
     private final Integer errors;
 
+    /** Punteggio ottenuto nella partita corrente */
     @SerializedName("score")
     private final Integer score;
 
@@ -64,6 +78,18 @@ public class LoginResponse {
         this.score = score;
     }
 
+    /**
+     * Crea una risposta di successo per login completato.
+     * 
+     * @param gameId id della partita corrente
+     * @param words lista di parole della partita corrente
+     * @param correctGroups lista di gruppi di parole già indovinate della partita corrente
+     * @param timeLeft tempo rimanente della partita corrente in millisecondi
+     * @param errors numero di errori già commessi nella partita corrente
+     * @param score punteggio ottenuto nella partita corrente
+     * 
+     * @return istanza con success=true e error=null
+     */
     public static LoginResponse success(
         Integer gameId,
         List<String> words,
@@ -72,26 +98,19 @@ public class LoginResponse {
         Integer errors,
         Integer score
     ) {
-        if (
-            gameId == null ||
-            words == null ||
-            correctGroups == null ||
-            timeLeft == null ||
-            errors == null ||
-            score == null
-        ) {
-            throw new IllegalArgumentException("Parameters can't be null");
-        }
-
-        // TO-DO
-
-        /*final int MIN_ID = 0;
-        final int MAX_ID = 911;
-        if (gameId ==)*/
+        // Check leggero
+        validateSuccess(gameId, words);
 
         return new LoginResponse(true, null, gameId, words, correctGroups, timeLeft, errors, score);
     }
 
+    /**
+     * Crea una risposta di errore per login fallito.
+     * 
+     * @param errorMsg messaggio d'errore descrittivo
+     * @return istanza con success=false, error=errorMsg e restante=null
+     * @throws IllegalArgumentException se errorMsg=null o vuoto
+     */
     public static LoginResponse error(String errorMsg) {
         if (errorMsg == null || errorMsg.trim().isEmpty()) {
             throw new IllegalArgumentException("Error message must be provided");
@@ -100,33 +119,21 @@ public class LoginResponse {
         return new LoginResponse(false, errorMsg, null, null, null, null, null, null);
     }
 
-    private void validateSuccess(
-        Integer gameId,
-        List<String> words,
-        List<List<String>> correctGroups,
-        Long timeLeft,
-        Integer errors,
-        Integer score
-    ) {
-        if (
-            gameId == null ||
-            (words == null || words.isEmpty()) ||
-            (correctGroups == null || correctGroups.isEmpty()) ||
-            timeLeft == null ||
-            errors == null ||
-            score == null
-        ) {
-            throw new IllegalArgumentException("Parameters can't be null");
-        }
-
+    /**
+     * Helper function per validazione veloce degli argomenti passati a success()
+     * 
+     * @param gameId id della partita corrente
+     * @param words lista di parole della partita corrente
+     * @throws IllegalArgumentException se gameId o words sono malformati
+     */
+    private static void validateSuccess(Integer gameId, List<String> words) {
         final int MIN_ID = 0;
         final int MAX_ID = 911;
-        if (gameId < MIN_ID || gameId > MAX_ID) {
+        if (gameId == null || gameId < MIN_ID || gameId > MAX_ID) {
             throw new IllegalArgumentException("gameId must be between 0 and 911");
         }
 
-
-
+        if (words == null || words.size() != 16) throw new IllegalArgumentException("words must contain 16 words");
     }
 
     // Getters
